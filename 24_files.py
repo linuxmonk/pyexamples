a91b64fd6e97fea5127448ff8a50ef0d0cd69318
linuxmonk <saikiran.gummaraj@gmail.com>
2020-09-25 11:42:08 +1000

    File operation examples

---
+++ 24_files.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+"""
+open a file using builtin 'open()' function
+"""
+
+# cat the contents of a file
+filename = '1_hello.py'
+print('Read contents from {} and write to screen'.format(filename))
+helloworld = open(filename)
+contents = helloworld.read()
+print(contents)
+
+"""
+read returns the entire contents of the file
+"""
+
+"""
+seek(offset) resets the file pointer to the offset
+tell() returns the current offset/position in the file
+"""
+
+print('Read more contents from {}'.format(filename))
+morecontents = helloworld.read()
+print(morecontents)
+print('current file pointer position is {}'.format(helloworld.tell()))
+
+"""
+read part of a file using read(N)
+where N is number of bytes
+"""
+helloworld.seek(0)
+magicbytes = helloworld.read(2)
+print("helloworld.read(2) returns type {}".format(type(magicbytes)))
+print("magicbytes: {}".format(magicbytes))
+
+"""
+close the file
+"""
+helloworld.close()
---
+++ 25_files.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+"""
+automatically close a file using 'with'
+"""
+
+filename = '1_hello.py'
+with open(filename) as helloworld:
+    print('is the file {} closed? {}'.format(filename, helloworld.closed))
+    print('contents:')
+    print('---------')
+    print(helloworld.read())
+    print('---------')
+
+print('file operation on {} complete. is file closed? {}'.format(filename, helloworld.closed))
+
+"""
+to read one line at a time there are two ways -
+
+1. use fp.readline()
+2. with open('filename') as file:
+    for line in file:
+        print(line)
+"""
+
+print("cat the contents of the file '{}'".format(filename))
+with open(filename) as pyfile:
+    for line in pyfile:
+        print(line.rstrip())
+
+"""
+open files in different modes 
+open(filename, mode)
+
+modes:
+    r - open in read mode (default)
+    w - open in write mode. truncate first
+    x - create a new file and open it for writing
+    a - open file for writing appending to it
+
+    + - read and write to the same file
+    b - binary mode
+    t - text mode (default)
+   
+   fp = open('/pictures/pic.jpg', rb)
+   fp.mode # gives the mode of the file
+"""
+
+print('writing something about a cat in cat.txt')
+with open('cat.txt', 'w') as txtfile:
+    txtfile.write('cat is a pet animal\n')
+    txtfile.write('i like dogs too\n')
+  
+print('reading cat.txt')
+with open('cat.txt') as rtxtfp:
+    print(rtxtfp.read())
+
+
+"""
+Use exceptions when dealing with file operations
+"""
+nofname = 'nofile.txt'
+try:
+    print('opening nofile.txt')
+    fp = open(nofname)
+    print('opeened nofile.txt, reading contents')
+    contents = fp.read()
+except:
+    print('file does not exist!')
